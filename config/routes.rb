ff013259229b64c6b5a3d2f348c6a6c019b83623
Goran-Arsov <arsphy@yahoo.com>
2019-07-30 09:36:36 +0200

    rake db:migrate

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,5 @@
 Rails.application.routes.draw do
   resources :magazines
   devise_for :users, :controllers => { registrations: 'registrations' }
-  root 'application#index'
-
+  root 'magazines#index'
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,12 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_07_30_072105) do
+ActiveRecord::Schema.define(version: 2019_07_30_073443) do
+
+  create_table "magazines", force: :cascade do |t|
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
