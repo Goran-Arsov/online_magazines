debd29aa56b005410f50f343b86fa324e0783a12
Goran-Arsov <arsphy@yahoo.com>
2019-07-30 10:11:32 +0200

    rails g controller subscriptions index show new edit

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,8 @@
 Rails.application.routes.draw do
+  get 'subscriptions/index'
+  get 'subscriptions/show'
+  get 'subscriptions/new'
+  get 'subscriptions/edit'
   resources :issues
   resources :magazines
   devise_for :users, :controllers => { registrations: 'registrations' }
