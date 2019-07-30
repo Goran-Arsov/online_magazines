117018a613d62a72660b0d0df879dec17f130c7b
Goran-Arsov <arsphy@yahoo.com>
2019-07-30 10:19:56 +0200

    Migrate Subscription model

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_07_30_073704) do
+ActiveRecord::Schema.define(version: 2019_07_30_080536) do
 
   create_table "issues", force: :cascade do |t|
     t.datetime "created_at", null: false
@@ -18,6 +18,11 @@ ActiveRecord::Schema.define(version: 2019_07_30_073704) do
   end
 
   create_table "magazines", force: :cascade do |t|
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "subscriptions", force: :cascade do |t|
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
