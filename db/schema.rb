dbd1345675563cd796eb18687d8473ed3856e454
Goran-Arsov <arsphy@yahoo.com>
2019-07-30 09:37:40 +0200

    rake db:migrate

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,12 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_07_30_073443) do
+ActiveRecord::Schema.define(version: 2019_07_30_073704) do
+
+  create_table "issues", force: :cascade do |t|
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "magazines", force: :cascade do |t|
     t.datetime "created_at", null: false
